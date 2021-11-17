464c84aa2a71de0094c58f14148d8dea4e826526
Farhazul Mullick <farhazulmullick99@gmail.com>
2021-11-17 17:37:54 +0530

    Added nav_drawer and video_layout

--- app/src/main/java/com/farhazulmullick/videoplayer/MainActivity.kt
+++ app/src/main/java/com/farhazulmullick/videoplayer/MainActivity.kt
@@ -5,8 +5,11 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.farhazulmullick.videoplayer.databinding.ActivityMainBinding
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +34,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         hostFragment(AllVideosFragment())
 
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setUptoolbar()
         binding.bottomNavView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.all_videos -> {
@@ -43,12 +57,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setUptoolbar() {
+        binding.toolBar.setNavigationOnClickListener {
+            binding.apply {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }else{
+                    drawerLayout.openDrawer(GravityCompat.START)
+                }
+            }
+        }
+    }
+
     override fun onStart() {
         super.onStart()
         if (isStoragePermissionGranted()) {
 
         } else {
-            Log.d(TAG , "Storage Permission Requested")
+            Log.d(TAG, "Storage Permission Requested")
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
@@ -72,16 +98,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            STORAGE_PERMISSION_REQ_CODE ->{
+        when (requestCode) {
+            STORAGE_PERMISSION_REQ_CODE -> {
                 grantResults.forEach {
-                    if (it != PackageManager.PERMISSION_GRANTED){
+                    if (it != PackageManager.PERMISSION_GRANTED) {
                         Log.d(TAG, "onRequestPermission() -> Storage Permission not Granted")
                         return
                     }
