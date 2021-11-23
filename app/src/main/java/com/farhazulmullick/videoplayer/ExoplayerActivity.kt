857c284af781ac317c0dd8a6bebfa216bfd5b5dc
Farhazul Mullick <farhazulmullick99@gmail.com>
2021-11-23 09:56:26 +0530

    Added exoplayer and custom cntrol buttons with singleton viewModelFactory and ViewModel

---
+++ app/src/main/java/com/farhazulmullick/videoplayer/ExoplayerActivity.kt
@@ -0,0 +1,11 @@
+package com.farhazulmullick.videoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExoplayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exoplayer)
+    }
+}
