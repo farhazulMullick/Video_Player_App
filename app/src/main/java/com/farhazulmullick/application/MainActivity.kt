e703e324fc0d35c688707f9b17c7a630d10a0cb3
farhazulmullick <farhazul.mullick@pw.live>
2023-08-15 16:34:03 +0530

    Implemented PlayStore app update checker.

--- app/src/main/java/com/farhazulmullick/application/MainActivity.kt
+++ app/src/main/java/com/farhazulmullick/application/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.farhazulmullick.application
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -9,8 +11,17 @@ import androidx.fragment.app.Fragment
 import com.farhazulmullick.core_ui.utils.checkForRequiredPermissions
 import com.farhazulmullick.feature.allvideos.ui.fragments.VideosFragment
 import com.farhazulmullick.feature.folders.ui.fragments.FoldersFragment
+import com.farhazulmullick.utils.toastS
 import com.farhazulmullick.videoplayer.R
 import com.farhazulmullick.videoplayer.databinding.ActivityMainBinding
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.InstallStatus
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -21,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
     private lateinit var toggle: ActionBarDrawerToggle
+
+    val appUpdateManager: AppUpdateManager by lazy { AppUpdateManagerFactory.create(this@MainActivity) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,26 +57,98 @@ class MainActivity : AppCompatActivity() {
                 R.id.all_videos -> {
                     hostFragment(VideosFragment())
                 }
+
                 R.id.all_folders -> {
                     hostFragment(FoldersFragment())
                 }
             }
             return@setOnItemSelectedListener true
         }
-        checkForRequiredPermissions(permissionTypes = listOf(com.farhazulmullick.core_ui.utils.PermissionType.NOTIFICATION))
-        hostFragment(VideosFragment())
+        checkForRequiredPermissions(
+            permissionTypes = listOf(com.farhazulmullick.core_ui.utils.PermissionType.NOTIFICATION),
+            onGranted = {
+                hostFragment(VideosFragment())
+            },
+            onDenied = {
+                hostFragment(VideosFragment())
+            })
+
+
+        checkIfAnyUpdateIsAvailable()
     }
 
     override fun onStart() {
         super.onStart()
     }
 
+    override fun onResume() {
+        super.onResume()
+        resumeIfUpdateInProgress()
+    }
+
+    private fun resumeIfUpdateInProgress() {
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                startUpdateFlow(appUpdateInfo)
+            }
+        }
+    }
+
+    private fun checkIfAnyUpdateIsAvailable() {
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+
+            if ((appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE))
+            ) {
+                startUpdateFlow(appUpdateInfo)
+            }
+        }
+    }
+
+    private val appUpdateResultLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {}
+    private fun startUpdateFlow(updateInfo: AppUpdateInfo) {
+        appUpdateManager.registerListener(listener)
+        appUpdateManager.startUpdateFlowForResult(
+            updateInfo,
+            appUpdateResultLauncher,
+            AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build()
+        )
+    }
+
+    private val listener = InstallStateUpdatedListener { state ->
+        when (state.installStatus()) {
+            InstallStatus.INSTALLED -> {
+                toastS(com.farhazulmullick.core_ui.R.string.app_updated)
+                unregisterUpdateListeners()
+            }
+            InstallStatus.FAILED -> {
+                toastS(com.farhazulmullick.core_ui.R.string.app_update_failed)
+                Log.e(TAG, "InstallStateUpdateListener: installError->${state.installErrorCode()}")
+                unregisterUpdateListeners()
+            }
+            InstallStatus.DOWNLOADING -> {}
+
+            else -> {}
+        }
+
+    }
+
+    private fun unregisterUpdateListeners() {
+        appUpdateManager.unregisterListener(listener)
+    }
+
     private fun setUptoolbar() {
         binding.toolBar.setNavigationOnClickListener {
             binding.apply {
-                if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
                     drawerLayout.closeDrawer(GravityCompat.START)
-                }else{
+                } else {
                     drawerLayout.openDrawer(GravityCompat.START)
                 }
             }
