30a2c733c96fbbf3d7edfa28f019371f3c6e76d3
farhazulmullick <farhazul.mullick@pw.live>
2023-08-19 13:20:51 +0530

    Added Mapper layer

---
+++ app/src/main/java/com/farhazulmullick/application/mapper/Mapper.kt
@@ -0,0 +1,42 @@
+package com.farhazulmullick.application.mapper
+
+import android.util.Log
+
+abstract class Mapper<Domain, Dto, Entity> {
+    private val TAG = "Mapper"
+
+    // DTO and Domain Mapping
+    abstract fun dtoToDomain(dto: Dto): Domain
+    abstract fun domainToDto(domain: Domain): Dto
+    fun dtoToDomain(dtoList: List<Dto>?): List<Domain> = try {
+        (dtoList ?: emptyList()).map(::dtoToDomain)
+    } catch (e: Exception) {
+        Log.e(TAG,"dtoToDomain(dtoList: List<Dto>) ${this.javaClass}", e)
+        emptyList()
+    }
+
+    fun domainToDto(domainList: List<Domain>?): List<Dto> = (domainList?: emptyList()).map(::domainToDto)
+
+    fun dtoToDomainWithheaderDate(dtoList: List<Dto>): List<Domain> = dtoList.map(::dtoToDomain)
+
+    // ****************** OPTIONAL USING ENTITY ***************************//
+    open fun entityToDomain(entity: Entity): Domain =
+        throw NotImplementedError("override and implement this method")
+
+    open fun domainToEntity(domain: Domain): Entity =
+        throw NotImplementedError("override and implement this method")
+
+    fun entityToDomain(domainList: List<Entity>): List<Domain> = domainList.map(::entityToDomain)
+    fun domainToEntity(domainList: List<Domain>): List<Entity> = domainList.map(::domainToEntity)
+
+    // ****************** OPTIONAL IF NO RELATION B/W DTO AND ENTITY ***************//
+    open fun entityToDto(entity: Entity): Dto =
+        throw NotImplementedError("override and implement this method")
+
+    open fun dtoToEntity(dto: Dto): Entity =
+        throw NotImplementedError("override and implement this method")
+
+    fun entityToDto(dtoList: List<Entity>): List<Dto> = dtoList.map(::entityToDto)
+    fun dtoToEntity(dtoList: List<Dto>): List<Entity> = dtoList.map(::dtoToEntity)
+
+}
