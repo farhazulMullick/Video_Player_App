260066af351f56974035c22c720c09dc29c72451
farhazulmullick <farhazul.mullick@pw.live>
2023-08-19 12:52:46 +0530

    Added columns in VideoDetails entity

--- app/src/main/java/com/farhazulmullick/data/local/recentwatch/entity/VideoDetails.kt
+++ app/src/main/java/com/farhazulmullick/data/local/recentwatch/entity/VideoDetails.kt
@@ -1,3 +1,28 @@
 package com.farhazulmullick.data.local.recentwatch.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.farhazulmullick.utils.EMPTY
+
+@Entity(tableName = "video_details")
 class VideoDetails {
+
+    @PrimaryKey(autoGenerate = true)
+    val videoId: String = String.EMPTY
+
+    @ColumnInfo(name = "video_title")
+    val videoTitle: String = String.EMPTY
+
+    @ColumnInfo("thumbnail")
+    val thumbnail: String = String.EMPTY
+
+    @ColumnInfo("video_duration")
+    val duration: Long = 0L
+
+    @ColumnInfo("last_watch_time")
+    val lastWatchTime: Long = 0L
+
+    @ColumnInfo("video_path")
+    val videoPath: String = String.EMPTY
 }
