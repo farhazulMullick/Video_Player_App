e90a32a5696ffb231842cbdac3c48cba29ccc3c6
farhazulmullick <farhazul.mullick@pw.live>
2023-10-10 20:52:35 +0530

    Revamped code of VideosFragment to compose

---
+++ app/src/main/java/com/farhazulmullick/feature/allvideos/ui/composable/AllVideosScreen.kt
@@ -0,0 +1,231 @@
+package com.farhazulmullick.feature.allvideos.ui.composable
+
+
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.farhazulmullick.core_ui.commoncomposable.YSpacer
+import com.farhazulmullick.core_ui.extensions.getBitmap
+import com.farhazulmullick.core_ui.utils.PermissionType
+import com.farhazulmullick.core_ui.utils.checkForRequiredPermissions
+import com.farhazulmullick.feature.allvideos.modal.Video
+import com.farhazulmullick.feature.allvideos.viewmodel.VideoViewModel
+import com.farhazulmullick.utils.toastS
+import com.farhazulmullick.videoplayer.R
+
+
+@Composable
+fun VideosScreen(
+    viewModel : VideoViewModel,
+    onVideoItemClicked: (Video) -> Unit
+) {
+    val videoList by viewModel.videoList.observeAsState()
+    val context = LocalContext.current
+
+    Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+        ContinueWatch(
+            videoList = videoList ?: emptyList(),
+            onVideoItemClicked = onVideoItemClicked
+        )
+
+        YSpacer(gap = 16.dp)
+
+        AllVideos(
+            videoList = videoList ?: emptyList(),
+            onVideoItemClicked = onVideoItemClicked
+        )
+
+    }
+
+}
+
+@Composable
+fun ContinueWatch(
+    videoList: List<Video>,
+    onVideoItemClicked: (Video) -> Unit
+) {
+    Column {
+
+        Text(
+            text = "Continue Watching",
+            style = MaterialTheme.typography.titleMedium)
+
+        YSpacer(gap = 16.dp)
+
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(space = 12.dp),
+            content = {
+            itemsIndexed (items = videoList){index: Int, item: Video ->
+                VideoUiH(videoItem = item, onVideoItemClicked = onVideoItemClicked)
+            }
+        })
+    }
+
+}
+
+@Composable
+fun VideoUiH(
+    videoItem: Video,
+    onVideoItemClicked: (Video) -> Unit = {}
+) {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .width(width = 110.dp)
+            .clickable { onVideoItemClicked(videoItem) }
+    ){
+        var thumbnail by remember { mutableStateOf<Bitmap?>(null) }
+        LaunchedEffect(key1 = true, block = {
+            thumbnail = videoItem.videoUri?.let{ context.getBitmap(uri = it) }
+        })
+
+        thumbnail?.let { bitmap ->
+            ThumbnailImage(thumbnail = bitmap)
+        }
+
+        Text(
+            modifier = Modifier
+                .width(width = 120.dp)
+                .padding(all = 8.dp),
+            text = videoItem.videoTitle,
+            maxLines = 2,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Composable
+fun VideoUiV(
+    videoItem: Video,
+    onVideoItemClicked: (Video) -> Unit = {}
+) {
+    val context = LocalContext.current
+    var thumbnail by remember { mutableStateOf<Bitmap?>(null) }
+    LaunchedEffect(key1 = true, block = {
+        thumbnail = videoItem.videoUri?.let { context.getBitmap(uri = it) }
+    })
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onVideoItemClicked(videoItem) }
+    ) {
+
+        thumbnail?.let {bitmap ->
+            ThumbnailImage(thumbnail = bitmap)
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(weight = 1f)
+                .padding(horizontal = 12.dp, vertical = 4.dp)
+        ) {
+            // video title
+            Text(
+                text = videoItem.videoTitle,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 2
+            )
+
+            // folder name
+            Text(
+                text = videoItem.videoFolderName,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray
+            )
+        }
+    }
+}
+
+@Composable
+fun ThumbnailImage(thumbnail: Bitmap) {
+    Box (
+        modifier = Modifier.wrapContentSize(),
+        contentAlignment = Alignment.Center){
+        Image(
+            modifier = Modifier
+                .height(height = 70.dp)
+                .aspectRatio(ratio = 1.571f)
+                .clip(shape = MaterialTheme.shapes.small),
+            bitmap = thumbnail.asImageBitmap(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Icon(
+            modifier = Modifier
+                .size(size = 20.dp)
+                .alpha(alpha = 0.8f),
+            painter = painterResource(R.drawable.ic_play_round_black),
+            contentDescription = null,
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+fun AllVideos(
+    videoList: List<Video>,
+    onVideoItemClicked: (Video) -> Unit = {}) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(12.dp), content = {
+        items(videoList) {
+            VideoUiV(videoItem = it, onVideoItemClicked = onVideoItemClicked )
+        }
+    })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewContinueLearning() {
+    ContinueWatch(videoList = listOf(
+            Video(videoTitle = "Video1"),
+            Video(videoTitle = "Video2"),
+            Video(videoTitle = "Video3"),
+            Video(videoTitle = "Video4"),
+            Video(videoTitle = "Video5"),
+        ),
+        onVideoItemClicked = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewVideoUiH() {
+    VideoUiH(videoItem = Video(videoTitle = "Video1"))
+}
--- app/src/main/java/com/farhazulmullick/feature/allvideos/ui/fragments/VideosFragment.kt
+++ app/src/main/java/com/farhazulmullick/feature/allvideos/ui/fragments/VideosFragment.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,6 +24,7 @@ import com.farhazulmullick.core_ui.utils.PermissionType
 import com.farhazulmullick.core_ui.utils.checkForRequiredPermissions
 import com.farhazulmullick.feature.allvideos.modal.Video
 import com.farhazulmullick.feature.allvideos.ui.composable.ContinueWatch
+import com.farhazulmullick.feature.allvideos.ui.composable.VideosScreen
 import com.farhazulmullick.utils.toast
 import com.farhazulmullick.utils.toastS
 import com.farhazulmullick.videoplayer.databinding.FragmentAllVideosBinding
@@ -47,59 +49,32 @@ class VideosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentAllVideosBinding.inflate(layoutInflater)
-        binding.viewModel = viewModel
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setUpRecyclerView()
-        viewModel.videoList.observe(viewLifecycleOwner) {
-            if (it.isNotEmpty()) {
-                videoAdapter.setVideos(it)
-            }
-        }
-
-        activity?.checkForRequiredPermissions(listOf(PermissionType.STORAGE),
-            onGranted = {
-                viewModel.fetchAllVideos()
-            },
-            onDenied = {
-                toastS("Please Grant media permissions to access files")
-            }
-        )
-
-        binding.continueCpv.setContent {
-            MaterialTheme {
-                val videoList by viewModel.videoList.observeAsState()
-                AnimatedVisibility(visible = videoList !=null && videoList!!.isNotEmpty()) {
-                    ContinueWatch(videoList = videoList ?: emptyList()) {video: Video ->
-                        context?.run {
-                            Intent(this@run, ExoplayerActivity::class.java).apply {
-                                this.putExtra("videoUri", video.videoPath)
-                                this.putExtra("videoTitle", video.videoTitle)
-                                ContextCompat.startActivity(this@run, this, null)
+        return if (context!=null) {
+            activity?.checkForRequiredPermissions(listOf(PermissionType.STORAGE),
+                onGranted = {
+                    viewModel.fetchAllVideos()
+                },
+                onDenied = {
+                    toastS("Please Grant media permissions to access files")
+                }
+            )
+            ComposeView(context = requireContext()).apply {
+                setContent{
+                    MaterialTheme {
+                        VideosScreen(
+                            viewModel = viewModel,
+                            onVideoItemClicked = {
+                                Intent(context, ExoplayerActivity::class.java).apply {
+                                    this.putExtra("videoUri", it.videoPath)
+                                    this.putExtra("videoTitle", it.videoTitle)
+                                    ContextCompat.startActivity(context, this, null)
+                                }
                             }
-                        }
+                        )
                     }
                 }
             }
-        }
-    }
-
-    private fun setUpRecyclerView() {
-        videoAdapter = VideoFragmentAdapter(requireContext(), viewModel)
-        binding.rvTotalVideos.apply {
-            adapter = videoAdapter
-            layoutManager = LinearLayoutManager(requireContext())
-
-            when (scrollState) {
-                RecyclerView.SCROLL_STATE_DRAGGING -> {
-                    context?.toast("Scrolling")
-                }
-            }
-        }
+        } else null
     }
 
     companion object {
